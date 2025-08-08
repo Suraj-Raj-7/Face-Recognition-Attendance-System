2366ff294d7cfb367cc533a197b8685ec2d2ea75
Suraj-Raj-7 <rajsuraj181002@gmail.com>
2025-08-08 14:59:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Create a virtual display for OpenCV to use, as Render's server doesn't have a screen
+# Create a virtual display for OpenCV to use
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 export DISPLAY=:99
 
