76be98dc3409f847aed0096c7e371d997394e9ce
Suraj-Raj-7 <rajsuraj181002@gmail.com>
2025-08-08 14:39:17 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Create a virtual display for OpenCV to use, as Render's server doesn't have a screen
+Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
+export DISPLAY=:99
+
+# Run the Flask app
+python app.py
